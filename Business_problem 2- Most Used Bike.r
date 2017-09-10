9ca35e95f35316dc69322d3a3f1c61af2f9261e8
gayatrik1234 <gayatrijb25@gmail.com>
2017-09-10 17:26:59 -0400

    Delete Business_problem 2- Most Used Bike.r

--- Business_problem 2- Most Used Bike.r
+++
@@ -1,9 +0,0 @@
-Data_Feb_2014 <- read.csv("/Users/Sushama/Desktop/Spring 2016/IS690Q Big Data/Project/Citibike data/2014-02 - Citi Bike trip data.csv")
-
-install.packages("plyr")
-install.packages("ggplot2")
-install.packages("reshape2")
-
-library(plyr)
-library(ggplot2)
-library(reshape2)
